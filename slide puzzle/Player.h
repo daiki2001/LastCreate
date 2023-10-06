358dec7dc29124077b01f2ccf7640a6d11508755
tani <k020g1507@g.neec.ac.jp>
2023-10-06 16:18:38 +0900

    プレイヤー挙動追加
    ・移動（ダッシュ可）
    ・ジャンプ
    ・投げる（関数のみ）
    ・捕る（関数のみ）

---
+++ slide puzzle/Player.h
@@ -0,0 +1,65 @@
+#pragma once
+#include"Object.h"
+#include"Sprite.h"
+#include<ParticleManager.h>
+#include<memory>
+#include<Audio.h>
+#include <array>
+#include <algorithm>
+#include <functional>
+
+class Player
+{
+private:
+	
+
+public:
+	//コンストラクタ
+	Player();
+	//デスコンストラクタ
+	~Player();
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Init();
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+private:
+	/// <summary>
+	/// 移動
+	/// </summary>
+	void Move();
+	/// <summary>
+	/// ジャンプ
+	/// </summary>
+	void Jump();
+	/// <summary>
+	/// 投げる
+	/// </summary>
+	void BallThrow();
+	/// <summary>
+	/// 捕る
+	/// </summary>
+	void BallCatch();
+
+private:
+	ObjectData pObject;                         //プレイヤーオブジェクト
+	Vec3 position = {};                         //位置
+	Vec3 rotation = {};
+
+	//接地フラグ
+	bool onGround_ = true;
+	// 落下ベクトル
+	Vec3 fallV_ = {};
+
+	// キャッチ関係
+	bool catchFlag_ = false;
+	int catchTimer_ = 0;
+};
+
