b551fb6739b46fa65e08078ee31696ddb95d0209
tani <k020g1507@g.neec.ac.jp>
2023-11-20 12:38:37 +0900

    適当に敵を動かせるようにした

--- slide puzzle/BaseEnemy.cpp
+++ slide puzzle/BaseEnemy.cpp
@@ -18,3 +18,41 @@ void BaseEnemy::Draw()
 {
 	Object::Draw(pObject, position, Vec3(1.0f, 1.0f, 1.0f), rotation);
 }
+
+void BaseEnemy::Move()
+{
+	if (!actFlag_)
+	{
+		maxTime_ = (rand() % 150) + 100;
+		act = static_cast<MoveAct>(rand() % 3);
+		timer_ = 0;
+		actFlag_ = true;
+	}
+	else
+	{
+		timer_++;
+	}
+
+	switch (act)
+	{
+	case BaseEnemy::Progress:
+		position.z += 0.5f;
+		break;
+	case BaseEnemy::Retreat:
+		position.z -= 0.5f;
+		break;
+	case BaseEnemy::Right:
+		position.x += 0.5f;
+		break;
+	case BaseEnemy::Left:
+		position.x -= 0.5f;
+		break;
+	default:
+		break;
+	}
+
+	if (timer_ > maxTime_)
+	{
+		actFlag_ = false;
+	}
+}
