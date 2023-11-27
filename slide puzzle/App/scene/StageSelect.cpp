5a72dec5ebb0fe13d2e6c3abcf427bd430a9f9eb
125moti <k020g1385@g.neec.ac.jp>
2023-11-27 16:26:54 +0900

    操作説明追加

--- slide puzzle/App/scene/StageSelect.cpp
+++ slide puzzle/App/scene/StageSelect.cpp
@@ -23,7 +23,7 @@ void StageSelect::Init()
 	FBXObject3d::SetLight(lightGroup.get());
 	Object::SetLight(lightGroup.get());
 
-
+	manipulate = Sprite::Get()->SpriteCreate(L"Resources/sprite/manipulate.png");
 	back = Sprite::Get()->SpriteCreate(L"Resources/gutitubo.png");
 	//オブジェクト生成
 	object = Shape::CreateOBJ("sphere");
@@ -54,6 +54,11 @@ void StageSelect::Update()
 void StageSelect::Draw()
 {
 	Sprite::Get()->Draw(back, Vec2(), static_cast<float>(window_width), static_cast<float>(window_height));
+
+	Vec2 size = { 700,500 };
+	float posX = static_cast<float>(window_width) / 2 - size.x / 2;
+	float posY = static_cast<float>(window_height) / 2 - size.y / 2;
+	Sprite::Get()->Draw(manipulate, Vec2(posX, posY), size.x, size.y);
 
 	Object::Draw(object, Vec3(), Vec3(1.0f, 1.0f, 1.0f), Vec3());
 
